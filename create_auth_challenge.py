11959ae66ec2c4666d3a014b9f85d7f438a41882
shichisan21 <photograph.by@gmail.com>
2023-07-29 22:40:51 +0900

    E-mail送信部分追加

--- create_auth_challenge.py
+++ create_auth_challenge.py
@@ -1,4 +1,9 @@
 import secrets
+import boto3
+
+# Create a new SES resource and specify a region.
+client = boto3.client('ses', region_name="us-west-2")
+
 
 def lambda_handler(event, context):
     if event['request']['session']:
@@ -8,13 +13,32 @@ def lambda_handler(event, context):
             else:
                 event['response']['issueTokens'] = False
         return event
-    
+
     # For the new authentication attempt, generate a new secret login code and mail it to the user
     # and setup the returned parameters to include the code parameter that the user will have to input
     secret_login_code = secrets.token_hex(20)
-    event['response']['privateChallengeParameters'] = {'secretLoginCode': secret_login_code}
+    event['response']['privateChallengeParameters'] = {
+        'secretLoginCode': secret_login_code}
     event['response']['challengeMetadata'] = 'CUSTOM_CHALLENGE'
 
-    # TODO: Add code to send the secret login code to the user's registered email
+    # Your registered user's email
+    user_email = 'USER_EMAIL'
+    response = client.send_email(
+        Destination={
+            'ToAddresses': [
+                user_email,
+            ],
+        },
+        Message={
+            'Body': {
+                'Text': {
+                    'Charset': 'UTF-8',
+                    'Data': 'Your secret login code is ' + secret_login_code,
+                },
+            },
+            'Subject': {'Charset': 'UTF-8', 'Data': 'Secret Login Code'},
+        },
+        Source='SENDER_EMAIL@DOMAIN.COM',  # Replace with your SES verified email
+    )
 
     return event
